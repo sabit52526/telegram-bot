e11796ec192db60c3240575dca4d6a469398c1ef
sabit52526 <sbt65057@gmail.com>
2026-09-24 11:54:56 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running 24/7!"
+
+def run():
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
+import os
 import time
 import sqlite3
 import telebot
